d3e883b96849ae8e75b93bfa874d03fe235a5051
FieldRayo <jesus.emir007@gmail.com>
2024-05-11 20:16:54 -0600

    Script para compilar todos los archivos .cpp de una carpeta

---
+++ compile_c++.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import sys, os, time
+import argparse
+
+args = sys.argv
+
+def main(file_path, exe, replace, show_data):
+    cpp_exist = False
+    
+    if os.path.exists(exe) and not replace:
+        x = input(f'--> El archivo "{exe}" ya existe, ¿quieres remplazarlo? (S/N)\n>>> ')
+        if x == 'S':
+            replace = True
+        else:
+            print(f'El archivo "{exe}" queda intacto')
+            exit()
+
+    if os.path.exist(exe) and replace:
+        os.remove(exe)
+
+
+    files = os.listdir(file_path)
+
+    command = f'g++ -o {exe} '
+
+    for f in files:
+        if (f.count('.cpp')):
+            command += file_path + '/' + f + ' '
+
+    os.system(command)
+
+    print('- Se han compilado correctamente todos los archivos -\n')
+    if show_data:
+        print('\n'.join(command[6::].split()))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Compila todos los archivos de una carpeta')
+
+    parser.add_argument('-p', '--path', help='Se ingresa la ruta absoluta de la carpeta')
+    parser.add_argument('-n', '--name', help='Nombre del ejecutable')
+    parser.add_argument('-r', '--replace', action='store_true', help='Remplaza el arhivo con el mismo nombre en caso de existir')
+    parser.add_argument('-s', '--show', action='store_true', help='Muestra los archivos que fueron compilados')
+
+    args = parser.parse_args()
+    
+    if not args.path or not args.name:
+        print('Hacen falta 1 o mas parametros. Consulta "compilecpp -h" para mas informacion')
+        exit()
+
+    main(args.path, args.name, args.replace, args.show)
