90aaa3413a2c521dac551c7a1688cebe6bbfe5dc
FieldRayo <jesus.emir007@gmail.com>
2024-05-15 21:07:25 -0600

    The parameters were adjusted so that it first receives the name and then the location of the folder. The parameters --execute and --parameter were added, which together can execute files after compiling. The --parameter parameter passes the parameters entered by the user to the execution command. The name 'exe' has been changed to 'name'.

--- compile_c++.py
+++ compile_c++.py
@@ -3,49 +3,51 @@
 import sys, os
 import argparse
 
-args = sys.argv
-
-def main(replace, show_data, file_path, exe, exclude, exlusive):
-    existing_file = os.path.exists(exe)
+def main(replace, show_data, file_path, name, exclude, execute, params):
+    existing_file = os.path.exists(name)
     
-    if existing_file and replace:
-        os.remove(exe)
-    elif existing_file:
-        print(f'Archivo existente: El archivo "{exe}" ya existe en la carpeta "file_path", si desea remplazarlo pruebe con el argumento --replace')
-        return
-
     files = os.listdir(file_path)
 
-    command = f'g++ -o {exe} '
-
+    command = f'g++ -o {name} '
+    
     for f in files:
         if '.cpp' in f and f not in exclude:
             command += os.path.abspath(file_path) + '/' + f + ' '
-
-    output = os.system(command)
     
+    # check the existence of a file with the same name 
+    if existing_file and replace:
+        os.remove(name)
+    elif existing_file:
+        print(f'Archivo existente: El archivo "{name}" ya existe en la carpeta "file_path", si desea remplazarlo pruebe con el argumento --replace')
+        return
+    
+    # Execute command
+    output = os.system(command)
     if output: return
 
     if show_data:
         print('\n'.join(command[6::].split()))
+
+    if execute:
+        print('\nEjecucion:')
+        output = os.system(f'./{name} {' '.join(params)}')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Compila todos los archivos de una carpeta')
     
     parser.add_argument('-r', '--replace', action='store_true', help='Remplaza el arhivo con el mismo nombre en caso de existir')
     parser.add_argument('-s', '--show', action='store_true', help='Muestra los archivos que fueron compilados')
+    parser.add_argument('-E', '--execute', action='store_true', help='Ejecuta el programa despues de compilarse')
     
     parser.add_argument('-e', '--exclude', nargs='+', help='Exluye los archivos que no van a ser compilados')
-    parser.add_argument('-E', '--exclusive', nargs='+', help='Elije los archivos que se van a compilar')
-    
-    parser.add_argument('path', nargs=1, help='Se ingresa la ruta absoluta de la carpeta')
+    parser.add_argument('-p', '--parameters', nargs='+', help='Manda los parametros ingresados al programa como input')    
+
     parser.add_argument('name', nargs=1, help='Nombre del ejecutable')
-    
+    parser.add_argument('path', nargs=1, help='Se ingresa la ruta absoluta de la carpeta')
+
     args = parser.parse_args()
 
     if not args.exclude:
         args.exclude = []
-    if not args.exclusive:
-        args.exclusive = []
     
-    main(args.replace, args.show, *args.path, *args.name, args.exclude, args.exclusive)
+    main(args.replace, args.show, *args.path, *args.name, args.exclude, args.execute, args.parameters)
